d974260ecd50a6702def0ee46c7ad902c96e688d
Bastien <nisole.bastien@outlook.fr>
2020-05-31 12:48:21 +0200

    Added new process for DATA predictions

---
+++ main/src/process/predict_process.py
@@ -0,0 +1,26 @@
+import logging
+from dataclasses import dataclass
+
+from main.src.importer.api_importer import ApiImporter
+from main.src.process.process_interface import Process
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class PredictProcess(Process):
+    importer_api: ApiImporter
+    name: str
+
+    def call_process(self) -> None:
+        logger.info(f'Call process {self.name}')
+
+        if self.force_process_execution:
+            self.start_safe_process()
+        else:
+            self.start_safe_process()
+
+        logger.info(f'End process {self.name}')
+
+    def start_safe_process(self):
+        logger.info('Working')
---
+++ main/src/service/predict_process_builder.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Dict, Any
+
+from main.src.importer.api_importer import ApiImporter
+from main.src.process.predict_process import PredictProcess
+from main.src.service.builder import ApiSourceBuilder
+
+
+@dataclass
+class PredictProcessBuilder:
+    source_config_api: Dict[Any, Any]
+    process_config: Dict[Any, Any]
+    force_process_execution: bool
+
+    def build_process(self) -> PredictProcess:
+
+        importer_api = self._build_impoter_api()
+        return PredictProcess(name=self.process_config['name'],
+                             use_asyncio=False,
+                             force_process_execution=self.force_process_execution,
+                             importer_api=importer_api)
+
+    def _build_impoter_api(self) -> ApiImporter:
+        builder = ApiSourceBuilder(self.source_config_api)
+        return builder.build_importer()
